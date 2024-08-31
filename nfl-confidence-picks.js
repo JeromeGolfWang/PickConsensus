786fd01c1e3d93e9434c3167016ce207821eca36
JeromeGolfWang <jay.finnigan@gmail.com>
2024-08-31 16:32:16 -0700

    Update nfl-confidence-picks.js

--- nfl-confidence-picks.js
+++ nfl-confidence-picks.js
@@ -177,7 +177,7 @@ async function savePicks() {
         }
     });
 
-    console.log("Picks saved:", picks);
+    console.log("Picks to be saved:", picks);
 
     // Send picks to the Cloudflare Worker (backend) for storage
     const response = await fetch('https://solitary-boat-e4cc.jay-finnigan.workers.dev/save-picks', {
